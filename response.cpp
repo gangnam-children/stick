c822f6a96c46adef06159ab5102a68b5af4e16af
InJung Chung <mu29gl@gmail.com>
2015-08-13 12:56:43 +0900

    Response class

---
+++ response.cpp
@@ -0,0 +1,9 @@
+//
+//  response.cpp
+//  server
+//
+//  Created by 정인중 on 2015. 8. 13..
+//  Copyright (c) 2015년 강남어린이들. All rights reserved.
+//
+
+#include "response.h"
---
+++ response.h
@@ -0,0 +1,27 @@
+//
+//  response.h
+//  server
+//
+//  Created by 정인중 on 2015. 8. 13..
+//  Copyright (c) 2015년 강남어린이들. All rights reserved.
+//
+
+#ifndef __server__response__
+#define __server__response__
+
+#include <string>
+using namespace std;
+
+class Response {
+private:
+    string route;
+    string method;
+    void* function;
+public:
+    Response(string, string, void*);
+    bool isMatch(string, string);
+    void* getFunction();
+};
+
+
+#endif /* defined(__server__response__) */
