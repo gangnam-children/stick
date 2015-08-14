d4300c5aa3efc3bd802d75e7e0d63796d7e33a50
InJung Chung <mu29gl@gmail.com>
2015-08-15 00:20:06 +0900

    String split function

---
+++ GlobalFunc.h
@@ -0,0 +1,23 @@
+#ifndef server_GlobalFunc_h
+#define server_GlobalFunc_h
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<std::string>
+split(const std::string & str, const std::string & delimiters) {
+    vector<std::string> v;
+    typename std::string::size_type start = 0;
+    auto pos = str.find_first_of(delimiters, start);
+    while (pos != std::string::npos) {
+        if (pos != start) // ignore empty tokens
+            v.emplace_back(str, start, pos - start);
+        start = pos + 1;
+        pos = str.find_first_of(delimiters, start);
+    }
+    if (start < str.length()) // ignore trailing delimiter
+        v.emplace_back(str, start, str.length() - start);
+    return v;
+}
+
+#endif
