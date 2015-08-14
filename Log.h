aad5a4b623aeca135a411432d1fc149f6a00f233
InJung Chung <mu29gl@gmail.com>
2015-08-15 03:38:00 +0900

    Log class

---
+++ Log.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+#define WRITELOG(fmt, ...)	Log::GetInstance()->trace( __FILE__, __LINE__, __PRETTY_FUNCTION__ , fmt, ##__VA_ARGS__);
+
+
+class Log
+{
+
+public:
+	static Log* GetInstance();
+	void trace( std::string file, int line, std::string func, std::string fmt, ...);
+private:
+	Log();
+	~Log();
+	Log(const Log&) = delete;
+	Log& operator=(const Log&) = delete;
+private:
+
+	std::ofstream mOutFile;
+};
