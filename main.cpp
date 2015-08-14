970ece0d8f8f94c0f013f7007051198dbef706b4
InJung Chung <mu29gl@gmail.com>
2015-08-15 03:38:59 +0900

    RESTful API

--- main.cpp
+++ main.cpp
@@ -6,44 +6,103 @@
 //  Copyright (c) 2015년 강남어린이들. All rights reserved.
 //
 
+#include <iostream>
 #include <stdlib.h>
+#include <string>
 #include "lightjson.h"
 #include "router.h"
 #include "UrlParser.h"
+#include "SimpleRedisClient.h"
+#include "redis.h"
 #include "fcgi_stdio.h"
 
+using namespace std;
 
 void userCreate(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>user_create</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = req.getBody()["key"].toString();
+    int score = req.getBody()["score"].toInt();
+    string member = req.getBody()["user"].toString();
+    int result = redis->zadd(key.c_str(), score, member.c_str());
+
+    Json::Writer writer;
+    Json::Object obj;
+    obj["result"] = result;
+    printf("Content-type: text/json \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void userRead(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>user_read</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = "test";//req.getParameter()["key"].toString();
+    string member = "injung";//req.getParameter()["user"].toString();
+    char* result = redis->zscore(key.c_str(), member.c_str());
+
+    Json::Writer writer;
+    Json::Object obj;
+    obj["score"] = result;
+    printf("Content-type: text/json \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void userUpdate(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>user_update</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = req.getBody()["key"].toString();
+    int score = req.getBody()["score"].toInt();
+    string member = req.getBody()["user"].toString();
+    int result = redis->zadd(key.c_str(), score, member.c_str());
+
+    Json::Writer writer;
+    Json::Object obj;
+    obj["result"] = result;
+    printf("Content-type: text/json \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void userDelete(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>user_delete</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = req.getBody()["key"].toString();
+    string member = req.getBody()["user"].toString();
+    int result = redis->zrem(key.c_str(), member.c_str());
+
+    Json::Writer writer;
+    Json::Object obj;
+    obj["result"] = result;
+    printf("Content-type: text/json \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void rankingIndividual(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>ranking_individual</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = "test";//req.getParameter()["key"].toString();
+    string member = "injung";//req.getParameter()["user"].toString();
+    int result = redis->zrevrank(key.c_str(), member.c_str()) + 1;
+
+    Json::Writer writer;
+    Json::Object obj;
+    obj["rank"] = result;
+    printf("Content-type: text/json \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void rankingList(Request req) {
-    printf("Content-type: text/html \r\n\r\n <h1>ranking_list</h1>");
+    Redis* redis = Redis::getInstance();
+    string key = "test";//req.getParameter()["key"].toString();
+    int start = 0;
+    int end = 1;
+    char** result = redis->zrevrange(key.c_str(), start, end);
+    int size = redis->zrevrangesize();
+
+    Json::Writer writer;
+    Json::Object obj;
+    for (int i = 0; i < size / 2; i++) {
+        obj[result[i * 2]] = result[i * 2 + 1];
+    }
+    printf("Content-type: text/html \r\n\r\n %s", writer.encode(obj).c_str());
 }
 
 void loadFunctions() {
     Response rUserCreate("/user", "POST", userCreate);
-    Response rUserRead("/user/:id", "GET", userRead);
+    Response rUserRead("/user", "GET", userRead);
     Response rUserUpdate("/user", "PUT", userUpdate);
     Response rUserDelete("/user", "DELETE", userDelete);
-    Response rRankingIndividual("/ranking/:start/:limit", "GET", rankingIndividual);
-    Response rRankingList("/ranking/:id", "GET", rankingList);
+    Response rRankingIndividual("/ranking/:id", "GET", rankingIndividual);
+    Response rRankingList("/ranking/:start/:end", "GET", rankingList);
     Router* router = Router::getInstance();
     router->add(rUserCreate);
     router->add(rUserRead);
