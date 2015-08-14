2aa1111ff297d405e531fd3a46e2cfd449b24445
InJung Chung <mu29gl@gmail.com>
2015-08-15 00:20:20 +0900

    Add responses

--- main.cpp
+++ main.cpp
@@ -6,14 +6,61 @@
 //  Copyright (c) 2015년 강남어린이들. All rights reserved.
 //
 
-#include <iostream>
-#include "json/json.h"
+#include <stdlib.h>
+#include "lightjson.h"
+#include "router.h"
+#include "UrlParser.h"
+#include "fcgi_stdio.h"
+
+
+void userCreate(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>user_create</h1>");
+}
+
+void userRead(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>user_read</h1>");
+}
+
+void userUpdate(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>user_update</h1>");
+}
+
+void userDelete(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>user_delete</h1>");
+}
+
+void rankingIndividual(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>ranking_individual</h1>");
+}
+
+void rankingList(Request req) {
+    printf("Content-type: text/html \r\n\r\n <h1>ranking_list</h1>");
+}
+
+void loadFunctions() {
+    Response rUserCreate("/user", "POST", userCreate);
+    Response rUserRead("/user/:id", "GET", userRead);
+    Response rUserUpdate("/user", "PUT", userUpdate);
+    Response rUserDelete("/user", "DELETE", userDelete);
+    Response rRankingIndividual("/ranking/:start/:limit", "GET", rankingIndividual);
+    Response rRankingList("/ranking/:id", "GET", rankingList);
+    Router* router = Router::getInstance();
+    router->add(rUserCreate);
+    router->add(rUserRead);
+    router->add(rUserUpdate);
+    router->add(rUserDelete);
+    router->add(rRankingIndividual);
+    router->add(rRankingList);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    Json::Value root;
-    root["z"] = 123;
-    root["x"] = "12";
-    std::cout << root["z"] <<" " << root["x"] << std::endl;
+    Router* router = Router::getInstance();
+    UrlParser* urlParser = UrlParser::GetInstance();
+    loadFunctions();
+    while (FCGI_Accept() >= 0) {
+        Request req = urlParser->GetRequest();
+        router->handle(req);
+        //printf("Content-type: text/html \r\n\r\n <h1>TEST</h1><p>FCGI</p>");
+    }
     return 0;
 }
