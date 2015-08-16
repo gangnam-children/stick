cccaaf10ddfc47f725f085c9fb31c98399d740ed
InJung Chung <mu29gl@gmail.com>
2015-08-16 15:51:50 +0900

    Add GlobalFunc header

--- GlobalFunc.cpp
+++ GlobalFunc.cpp
@@ -1,11 +1,19 @@
+//
+//  GlobalFunc.h
+//  server
+//
+//  Created by 정인중 on 2015. 8. 14..
+//  Copyright (c) 2015년 강남어린이들. All rights reserved.
+//
+
 #ifndef server_GlobalFunc_h
 #define server_GlobalFunc_h
+
 #include <vector>
 #include <string>
 using namespace std;
 
-vector<std::string>
-split(const std::string & str, const std::string & delimiters) {
+vector<std::string> split(const std::string & str, const std::string & delimiters) {
     vector<std::string> v;
     typename std::string::size_type start = 0;
     auto pos = str.find_first_of(delimiters, start);
